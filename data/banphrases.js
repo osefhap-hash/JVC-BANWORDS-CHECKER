5e9c4cddbab78fec479b4126d3d32039d6af5e0f
osefhap-hash <osefhap@gmail.com>
2026-09-19 05:42:38 +0200

    Add banphrases dictionary for filtering content

---
+++ data/banphrases.js
@@ -0,0 +1,20 @@
+const banphrases_dictionary = {
+	"G" : new Set([
+		"grand remplacement",
+	]),
+	"J" : new Set([
+		"jeunesse et valeurs",
+	]),
+	"K" : new Set([
+		"kali yuga",
+	]),
+	"L" : new Set([
+		"laetitia avia", /* mais Lætitia Avia n'est pas censuré */
+	]),
+	"M" : new Set([
+		"marc ladreit de lacharriere",
+	]),
+	"T" : new Set([
+		"ta gueule", /* mais pas gueule tout seul, ni gueuler */
+	]),
+} ;
